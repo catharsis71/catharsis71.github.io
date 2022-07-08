61e6e2357aad38289fee1c9d9a8e00d99775fea3
cracky-chan <70603733+catharsis71@users.noreply.github.com>
2022-07-08 14:41:43 -0500

    .

--- slide.js
+++ slide.js
@@ -16,6 +16,6 @@ function showSlides() {
 function playvid() {
   document.getElementById('playbutton').style.display = "none";
   document.getElementById('slideshow-container').style.display = "none";
-  document.getElementById('slideshow-container').style.display = "inline";
+  document.getElementById('this').style.display = "default";
   document.getElementById('this').play();
 }
